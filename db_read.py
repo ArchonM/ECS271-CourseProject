e7244a19933a60f9c7e09969673653a53fd608dd
Asmita A <asmitaa@TranquilTurkey.local>
2022-11-30 02:58:30 -0800

    db_read.py

--- db_read.py
+++ db_read.py
@@ -10,10 +10,14 @@ try :
     cursor.execute(sql_query)
     list_of_tables = cursor.fetchall()
     print(list_of_tables)
-    df = pd.read_sql_query("SELECT * from acfg", con)
+    # df = pd.read_sql_query("SELECT * from acfg", con)
 
-    # Verify that result of SQL query is stored in the dataframe
-    print(df.head())
+    # # Verify that result of SQL query is stored in the dataframe
+    # print(df.head())
+    for table_name in list_of_tables :  #Source - https://stackoverflow.com/questions/305378/list-of-tables-db-schema-dump-etc-using-the-python-sqlite3-api/33100538#33100538
+        table_name = table_name[0]
+        table = pd.read_sql_query("SELECT * from %s" % table_name, db)
+        table.to_csv(table_name + '.csv', index_label='index')
 
 
 except sqlite3.Error as error:
