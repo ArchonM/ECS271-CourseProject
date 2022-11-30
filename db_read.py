d8d3776188cd5497766e5387be3428b8168fd772
Asmita A <asmitaa@TranquilTurkey.local>
2022-11-30 01:31:52 -0800

    db_read.py

--- db_read.py
+++ db_read.py
@@ -1,10 +1,20 @@
 import sqlite3
 import pandas as pd
 
-con = sqlite3.connect("OpenSSL_dataset.db")
-sql_query = """SELECT name FROM sqlite_master  
-  WHERE type='table';"""
-cursor = con.cursor()
-cursor.execute(sql_query)
-list_of_tables = cursor.fetchall()
-print(list_of_tables)
+db_path = "/Users/asmitaa/binarySimiliarityProject/OpenSSL_dataset.db"
+try : 
+    con = sqlite3.connect(db_path)
+    sql_query = """SELECT name FROM sqlite_master  
+    WHERE type='table';"""
+    cursor = con.cursor()
+    cursor.execute(sql_query)
+    list_of_tables = cursor.fetchall()
+    print(list_of_tables)
+
+except sqlite3.Error as error:
+    print("Failed to execute above queries", error)
+
+finally:
+    if con:
+      con.close()
+      print("the sqlite connection is closed")
