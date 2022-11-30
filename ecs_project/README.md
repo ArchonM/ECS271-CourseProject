c9019b695949828d85e1654e4c6bdf2feec15066
Asmita A <asmitaa@campus-109-071.ucdavis.edu>
2022-11-30 10:20:27 -0800

    ecs_project folder added

---
+++ ecs_project/README.md
@@ -0,0 +1,3 @@
+# CSV Files
+* All CSV Files are generated from openssl.db file via db_read.py script.
+* The generated CSVs have been shared by gdrive.
---
+++ ecs_project/db_read.py
@@ -0,0 +1,29 @@
+import sqlite3
+import pandas as pd
+
+db_path = "/Users/asmitaa/binarySimiliarityProject/OpenSSL_dataset.db"
+try : 
+    con = sqlite3.connect(db_path)
+    sql_query = """SELECT name FROM sqlite_master  
+    WHERE type='table';"""
+    cursor = con.cursor()
+    cursor.execute(sql_query)
+    list_of_tables = cursor.fetchall()
+    print(list_of_tables)
+    # df = pd.read_sql_query("SELECT * from acfg", con)
+
+    # # Verify that result of SQL query is stored in the dataframe
+    # print(df.head())
+    for table_name in list_of_tables :  #Source - https://stackoverflow.com/questions/305378/list-of-tables-db-schema-dump-etc-using-the-python-sqlite3-api/33100538#33100538
+        table_name = table_name[0]
+        table = pd.read_sql_query("SELECT * from %s" % table_name, con)
+        table.to_csv(table_name + '.csv', index_label='index')
+
+
+except sqlite3.Error as error:
+    print("Failed to execute above queries", error)
+
+finally:
+    if con:
+      con.close()
+      print("the sqlite connection is closed")
