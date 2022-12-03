d7a0787c84bf2e61656ad230ca4b522fb3cd8945
asmitaj08 <asmita.jha82@gmail.com>
2022-12-03 10:32:10 -0800

    get files update

--- ecs_project/get_test_false.py
+++ ecs_project/get_test_false.py
@@ -38,13 +38,13 @@ list_train_true_pairs = []
 list_train_false_pairs = []
 
 # These are for training pairs, too long , comment when not needed
-# for i in range(0, df_train_pairs_shape[0]) :
-#     list_train_true_pairs.extend(literal_eval(df_train_pairs.loc[i]['true_pair'])) # Contains the list of ids of true_pairs (from test dataset)
+for i in range(0, df_train_pairs_shape[0]) :
+    list_train_true_pairs.extend(literal_eval(df_train_pairs.loc[i]['true_pair'])) # Contains the list of ids of true_pairs (from test dataset)
 
-#     list_train_false_pairs.extend(literal_eval(df_train_pairs.loc[i]['false_pair'])) # Contains the list of ids of false_pairs (from test dataset)
+    list_train_false_pairs.extend(literal_eval(df_train_pairs.loc[i]['false_pair'])) # Contains the list of ids of false_pairs (from test dataset)
 
-# print("list_train_true_pairs len : ", len(list_train_true_pairs))
-# print("list_train_false_pairs len : ", len(list_train_false_pairs))
+print("list_train_true_pairs len : ", len(list_train_true_pairs))
+print("list_train_false_pairs len : ", len(list_train_false_pairs))
 
 # Just to test
 # ele = list_test_true_pairs[3][0]
@@ -113,55 +113,71 @@ test_true_paired_features = [] # contains list of mnemonic features from two ids
 test_true_paired_feature_id = []
 test_true_paired_feature_vec = []
 test_true_id_pair_list = []
+similar_count = 0
 for pairs in list_test_false_pairs :
     pair_feature_list = []
     pair_feature_id_list = []
     pair_feature_vec_list = []
-    test_true_id_pair_list.append(pairs)
-    # print(pairs)
-    for ids in pairs :
-        id_feature = []
-        # print(ids)
-        val = df_cfg.loc[df_cfg['id'] == ids]['lstm_cfg']
-        lstm_cfg_feature = val.iloc[len(val)-1]
-        lstm_cfg_feature_dict = json.loads(lstm_cfg_feature)
-        # print(lstm_cfg_feature_dict)
-        for nodes in lstm_cfg_feature_dict['nodes'] :
-            # print(nodes)
-            if nodes['features'] : 
-                # print(nodes['features'])
-                id_feature.extend(nodes['features'])
-                # print(id_feature)
-        pair_feature_list.append(id_feature)
-        normalized_feature_ids = normalize(convert_to_ids(id_feature))
-        pair_feature_id_list.append(normalized_feature_ids)
-        pair_feature_vec_list.append(convert_to_vec(normalized_feature_ids))
-        # print(pair_feature_list)
-        # print("~~~~~~~~~~~~~~~~~")
-    test_true_paired_features.append(pair_feature_list)
-    test_true_paired_feature_id.append(pair_feature_vec_list)
-    test_true_paired_feature_vec.append(pair_feature_vec_list)
+
+    if(([pairs[0], pairs[1]] in test_true_id_pair_list) or ([pairs[1], pairs[0]] in test_true_id_pair_list)):
+        similar_count += 1
+    
+    else :
+        test_true_id_pair_list.append(pairs)
+        # print(pairs)
+        for ids in pairs :
+            id_feature = []
+            # print(ids)
+            val = df_cfg.loc[df_cfg['id'] == ids]['lstm_cfg']
+            lstm_cfg_feature = val.iloc[len(val)-1]
+            lstm_cfg_feature_dict = json.loads(lstm_cfg_feature)
+            # print(lstm_cfg_feature_dict)
+            for nodes in lstm_cfg_feature_dict['nodes'] :
+                # print(nodes)
+                if nodes['features'] : 
+                    # print(nodes['features'])
+                    id_feature.extend(nodes['features'])
+                    # print(id_feature)
+            pair_feature_list.append(id_feature)
+            normalized_feature_ids = normalize(convert_to_ids(id_feature))
+            pair_feature_id_list.append(normalized_feature_ids)
+            pair_feature_vec_list.append(convert_to_vec(normalized_feature_ids))
+            # print(pair_feature_list)
+            # print("~~~~~~~~~~~~~~~~~")
+        test_true_paired_features.append(pair_feature_list)
+        test_true_paired_feature_id.append(pair_feature_vec_list)
+        test_true_paired_feature_vec.append(pair_feature_vec_list)
     
     # print("************")
 # print(test_true_paired_features)
 # print(test_true_paired_feature_vec)
+print(" similar ", similar_count)
 print(len(test_true_paired_features))
 print(len(test_true_paired_feature_id))
 print(len(test_true_id_pair_list))
+print(len(test_true_paired_feature_vec))
 assert(len(test_true_paired_features) == len(test_true_id_pair_list))
-test_true_pair_labels = [1]*len(test_true_paired_features)
+test_true_pair_labels = [0]*len(test_true_paired_features)   # False
 
 test_true_pair_dict = {'ids':test_true_id_pair_list , 'features': test_true_paired_features, 'feature_ids':test_true_paired_feature_id ,'labels': test_true_pair_labels}
 test_true_pair_dict_vec = {'feature_vec' : test_true_paired_feature_vec ,'labels': test_true_pair_labels}
-test_true_pair_df = pd.DataFrame(test_true_pair_dict)
-test_true_pair_df_vec = pd.DataFrame(test_true_pair_dict_vec)
+test_true_pair_dict_drop = test_true_pair_dict.drop_duplicates()
+test_true_pair_dict_vec_drop = test_true_pair_dict_vec.drop_duplicates()
+test_true_pair_df = pd.DataFrame(test_true_pair_dict_drop)
+test_true_pair_df_vec = pd.DataFrame(test_true_pair_dict_vec_drop)
 print(test_true_pair_df)
+print(len(test_true_pair_dict))
+print(len(test_true_pair_dict_vec))
+print(len(test_true_pair_dict_drop))
+print(len(test_true_pair_dict_vec_drop))
 print(len(test_true_pair_df))
-print(len(test_true_paired_feature_vec))
-print(len(test_true_paired_feature_vec[0]))
-print(len(test_true_paired_feature_vec[0][0]))
-print((test_true_paired_feature_vec[0][0][0].shape))
-test_true_pair_df.to_csv("test_false_pairs.csv", sep=',') # geneal info
-test_true_pair_df_vec.to_csv("test_false_pairs_vec.csv", sep=',')
+print(len(test_true_pair_df_vec))
+
+# print(len(test_true_paired_feature_vec[0]))
+# print(len(test_true_paired_feature_vec[0][0]))
+# print((test_true_paired_feature_vec[0][0][0].shape))
+test_true_pair_df_vec.to_csv("test_false_new_pairs_vec.csv", sep=',')
+test_true_pair_df.to_csv("test_flase_new_pairs.csv", sep=',') # geneal info
+
 
 
